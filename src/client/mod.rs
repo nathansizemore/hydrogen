08188953ec4902990c235d0ee8b76adfe25b8908
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-07-24 10:04:13 -0400

    Debugging stuff

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -36,7 +36,7 @@ pub extern "C" fn connect(address: *const c_char,
     on_connect_handler: extern fn(),
     on_disconnect_handler: extern fn()) -> c_int {
 
-    1 as c_int;
+    1 as c_int
 
 
     // let mut r_address;
