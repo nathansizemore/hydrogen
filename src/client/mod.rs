b7194f5a8c2e9ebdfb23631de9da4a0ec09cd9fc
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-07-27 15:50:24 -0400

    Ahhh

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -35,29 +35,21 @@ pub extern "C" fn start(address: *const c_char,
 
     println!("Rust - start()");
 
-    on_connect_handler();
+    let mut r_address;
+    unsafe {
+        r_address = CStr::from_ptr(address);
+    }
+    let s_address = r_address.to_bytes();
+    let host_address = match str::from_utf8(s_address) {
+        Ok(safe_str) => safe_str,
+        Err(_) => {
+            println!("Invalid host address");
+            return -1 as c_int;
+        }
+    };
 
+    println!("Rust - address: {}", host_address);
 
-
-
-
-
-    //
-    // let mut r_address;
-    // unsafe {
-    //     r_address = CStr::from_ptr(address);
-    // }
-    // let s_address = r_address.to_bytes();
-    // let host_address = match str::from_utf8(s_address) {
-    //     Ok(safe_str) => safe_str,
-    //     Err(_) => {
-    //         println!("Invalid host address");
-    //         return -1 as c_int;
-    //     }
-    // };
-    //
-    // println!("Rust - address: {}", host_address);
-    //
     // // Create and register a way to kill this client
     // let (k_tx, kill_rx): (Sender<()>, Receiver<()>) = channel();
     // let kill_tx = k_tx.clone();
