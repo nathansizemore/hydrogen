014c0fccac0dfd4e19d292ff1bc4a8a4a5803453
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-10-14 12:19:28 -0400

    Removed dual-license incompatibility clause

--- src/server/resources.rs
+++ src/server/resources.rs
@@ -6,10 +6,6 @@
 // distributed with this file, You can
 // obtain one at
 // http://mozilla.org/MPL/2.0/.
-//
-// This Source Code Form is "Incompatible
-// With Secondary Licenses", as defined by
-// the Mozilla Public License, v. 2.0.
 
 
 use std::sync::Arc;
