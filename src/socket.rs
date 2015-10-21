e34e3ca0d9928ca0061b906b92531800fc4869ac
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-10-20 22:50:04 -0400

    License text formatting changed.

--- src/socket.rs
+++ src/socket.rs
@@ -1,10 +1,8 @@
 // Copyright 2015 Nathan Sizemore <nathanrsizemore@gmail.com>
 //
-// This Source Code Form is subject to the
-// terms of the Mozilla Public License, v.
-// 2.0. If a copy of the MPL was not
-// distributed with this file, You can
-// obtain one at
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at
 // http://mozilla.org/MPL/2.0/.
 
 
