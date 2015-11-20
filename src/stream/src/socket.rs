47a49b3ed97eb325a760e4a47c79f935ac069be4
Nathan <nathanrsizemore@gmail.com>
2015-11-20 01:43:39 -0500

    Socket complete

--- src/stream/src/socket.rs
+++ src/stream/src/socket.rs
@@ -10,17 +10,48 @@ extern crate libc;
 extern crate errno;
 
 
+use std::{fmt, error};
 use std::io::{Read, Write, Error, ErrorKind};
-use self::libc::{c_int, c_void, size_t, ssize_t};
 
+use self::libc::{c_int, c_void};
 
-extern "C" {
-    fn read(fd: c_int, buffer: *mut c_void, count: size_t) -> ssize_t;
-    fn write(fd: c_int, buffer: *const c_void, cout: size_t) -> ssize_t;
-}
 
 pub struct Socket {
-    fd: c_int
+    pub fd: c_int
+}
+
+impl Read for Socket {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+        if buf.len() < 1 {
+            return Err(Error::new(ErrorKind::Other, ReadError::InvalidBuffer))
+        }
+
+        let result = unsafe {
+            libc::read(self.fd, buf as *mut _ as *mut c_void, buf.len())
+        };
+
+        if result < 0 {
+            return Err(Error::new(ErrorKind::Other, ReadError::from_errno(result as i32)))
+        }
+
+        Ok(result as usize)
+    }
+}
+
+impl Write for Socket {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+        let result = unsafe {
+            libc::write(self.fd, buf as *const _ as *const c_void, buf.len())
+        };
+
+        if result < 0 {
+            return Err(Error::new(ErrorKind::Other, WriteError::from_errno(result as i32)))
+        }
+
+        Ok(result as usize)
+    }
+
+    fn flush(&mut self) -> Result<(), Error> { Ok(()) }
 }
 
 
@@ -55,7 +86,9 @@ pub enum ReadError {
     /// End of file has been reached or fd is connected to a TCP socket where the other end has
     /// been closed, but the write command has not had enough time to finish writing to the
     /// internal buffer. No more can be read from this socket.
-    Closed
+    Closed,
+    /// Passed buffer length was less than 1
+    InvalidBuffer
 }
 
 impl ReadError {
@@ -71,26 +104,112 @@ impl ReadError {
             libc::ECONNRESET => ReadError::Closed,
             libc::EOF => ReadError::Closed,
             libc::EWOULDBLOCK => ReadError::WouldBlock,
-            libc::EAGAIN => ReadError::WouldBlock,
 
             _ => panic!("Unexpected errno during read: {}", errno)
         }
     }
 }
 
-impl
-
-impl Read for Socket {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, ReadError> {
-        if buf.len() < 1 {
-            return Err(ReadError::InvalidBuffer)
+impl fmt::Display for ReadError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ReadError::NoMemory => "NoMemory".fmt(f),
+            ReadError::WouldBlock => "WouldBlock".fmt(f),
+            ReadError::InvalidFd => "InvalidFd".fmt(f),
+            ReadError::MemAccess => "MemAccess".fmt(f),
+            ReadError::Signal => "Signal".fmt(f),
+            ReadError::Io => "Io".fmt(f),
+            ReadError::Closed => "Closed".fmt(f),
+            ReadError::InvalidBuffer => "InvalidBuffer".fmt(f)
         }
+    }
+}
 
-        let result = unsafe { read(self.fd, buf as *mut c_void, buf.len()) };
-        if result < 0 {
-            Err(ReadError::from_errno(result))
+impl error::Error for ReadError {
+    fn description(&self) -> &str {
+        match *self {
+            ReadError::NoMemory => "NoMemory",
+            ReadError::WouldBlock => "WouldBlock",
+            ReadError::InvalidFd => "InvalidFd",
+            ReadError::MemAccess => "MemAccess",
+            ReadError::Signal => "Signal",
+            ReadError::Io => "Io",
+            ReadError::Closed => "Closed",
+            ReadError::InvalidBuffer => "InvalidBuffer"
         }
+    }
+}
 
-        Ok(result as usize)
+
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum WriteError {
+    /// The file descriptor fd refers to a file other than a socket and has been
+    /// marked nonblocking (O_NONBLOCK), and the read would block.
+    WouldBlock,
+    /// This could be several things:
+    ///
+    /// * Fd is not a valid file descriptor.
+    /// * Fd is not open for writing.
+    /// * Fd is attached to an object which is unsuitable for writing
+    /// * The file was opened with the `O_DIRECT` flag, and either the address specified in buf,
+    ///   the value specified in count, or the current file offset is not suitably aligned.
+    InvalidFd,
+    /// buf is outside your accessible address space.
+    MemAccess,
+    /// The call was interrupted by a signal before any data was read.
+    /// [see signal(7)][signal7]
+    /// [signal7]: http://man7.org/linux/man-pages/man7/signal.7.html
+    Signal,
+    /// A low-level I/O error occurred while modifying the inode.
+    Io,
+    /// fd is connected to a pipe or socket whose reading end is closed. When this happens
+    /// the writing process will also receive a SIGPIPE signal. (Thus, the write return value
+    /// is seen only if the program catches, blocks or ignores this signal.) Or, fd is connected
+    /// to a TCP socket where the other end has been closed, but the write command has not had
+    /// enough time to finish writing to the internal buffer.
+    Closed
+}
+
+impl WriteError {
+    pub fn from_errno(errno: i32) -> WriteError {
+        match errno {
+            libc::EAGAIN => WriteError::WouldBlock,
+            libc::EBADF => WriteError::InvalidFd,
+            libc::EFAULT => WriteError::MemAccess,
+            libc::EINTR => WriteError::Signal,
+            libc::EINVAL => WriteError::InvalidFd,
+            libc::EIO => WriteError::Io,
+            libc::EPIPE => WriteError::Closed,
+            libc::ECONNRESET => WriteError::Closed,
+
+            _ => panic!("Unexpected errno during read: {}", errno)
+        }
+    }
+}
+
+impl fmt::Display for WriteError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            WriteError::WouldBlock => "WouldBlock".fmt(f),
+            WriteError::InvalidFd => "InvalidFd".fmt(f),
+            WriteError::MemAccess => "MemAccess".fmt(f),
+            WriteError::Signal => "Signal".fmt(f),
+            WriteError::Io => "Io".fmt(f),
+            WriteError::Closed => "Closed".fmt(f)
+        }
+    }
+}
+
+impl error::Error for WriteError {
+    fn description(&self) -> &str {
+        match *self {
+            WriteError::WouldBlock => "WouldBlock",
+            WriteError::InvalidFd => "InvalidFd",
+            WriteError::MemAccess => "MemAccess",
+            WriteError::Signal => "Signal",
+            WriteError::Io => "Io",
+            WriteError::Closed => "Closed"
+        }
     }
 }
