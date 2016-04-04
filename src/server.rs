d4438a42267826faceac7426cf8e40f4272a1e57
nathan <nathanrsizemore@gmail.com>
2016-04-04 15:08:53 -0400

    Remote machine compiling

--- src/server.rs
+++ src/server.rs
@@ -210,7 +210,7 @@ unsafe fn event_loop(new_connections: NewConnectionSlab,
               threads: usize)
 {
     // Maximum number of events returned from epoll_wait
-    const MAX_EVENTS: usize = 100;
+    const MAX_EVENTS: i32 = 100;
     const MAX_WAIT: i32 = 100; // Milliseconds
 
     // Attempt to create an epoll instance
@@ -251,15 +251,15 @@ unsafe fn event_loop(new_connections: NewConnectionSlab,
         prepare_connections_for_epoll_wait(epfd, &connection_slab);
 
         // Check for any new events
-        match libc::epoll_wait(epfd, event_buffer.as_mut_ptr(), MAX_EVENTS, MAX_WAIT) {
-            Ok(num_events) => {
-                update_io_events(&connection_slab, &event_buffer[0..num_events]);
-            }
-            Err(e) => {
-                error!("During epoll::wait(): {}", e);
-                panic!()
-            }
-        };
+        let result = libc::epoll_wait(epfd, event_buffer.as_mut_ptr(), MAX_EVENTS, MAX_WAIT);
+        if result < 0 {
+            let err = Error::from_raw_os_error(result as i32);
+            error!("During epoll_wait: {}", err);
+            panic!();
+        }
+
+        let num_events = result;
+        update_io_events(&connection_slab, &event_buffer[0..num_events]);
     }
 }
 
